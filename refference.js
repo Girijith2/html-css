c344bdc46b70de46902122d2935279bfa299c3a6
Girijith <sanjithgirijith@gmail.com>
2025-02-25 19:34:16 +0530

    Add files via upload

---
+++ refference.js
@@ -0,0 +1,14 @@
+var h1 = document.getElementById("h1")
+var h11 = document.getElementById("h11")
+var h111 = document.getElementById("h111")
+var p1 = document.getElementById("p1")
+
+function update(){
+    h1.innerHTML = "Girijith K"
+    h11.innerHTML = "junior web developer"
+    h111.innerHTML = "and as a fresher for work"
+    p1.innerHTML = "my dream is to start a won company,and i need to know about the company how it's working and want to gain more english knowledge to speak well" 
+    p1.style.width= "30%";
+    alert("content will change")
+}
+
